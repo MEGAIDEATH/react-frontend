f9deecd72d0731da0e83f980add6ae3c1306e17e
MEGAIDEATH <thedem.80@gmail.com>
2026-02-10 23:58:45 +0300

    frontend setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/users')
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data from Backend</h1>
+      {data.map((item, index) => (
+        <p key={index}>{item.name || JSON.stringify(item)}</p>
+      ))}
     </div>
   );
 }
